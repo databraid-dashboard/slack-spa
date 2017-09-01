1ce9b21781120973f3c2d095489f522854ebff93
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-08-31 22:54:27 -0700

    Configure fetch methods to communicate with api and more concise

--- src/Utils/Api.js
+++ src/Utils/Api.js
@@ -1,27 +1,22 @@
 // @flow
 
-// TODO: Needs to be moved to environmental variables file
-// const PATH = 'https://databraid.localtunnel.me';
-
-// TODO: remove when no longer needed
-const PATH = 'http://localhost:4000/';
+// TODO: Get to work with .env file
+const PATH = 'http://localhost:8001';
+// const PATH = process.env.SLACK_API_URL;
 
 /* eslint func-names: ["error", "never"] */
 function fetchRequest(path) {
-  return fetch(path);
+  return fetch(path).then(response => response.json());
 }
 
 export default class SLACK_API {
-
   static async fetchRequestChannels() {
-    const response = await fetchRequest(`${PATH}channels`);
-    const channels = await response.json();
+    const channels = await fetchRequest(`${PATH}/channels`);
     return channels;
   }
 
-  static async fetchRequestMessagesForChannel() {
-    const response = await fetchRequest(`${PATH}messages`);
-    const messages = await response.json();
+  static async fetchRequestMessagesForChannel(channel) {
+    const messages = await fetchRequest(`${PATH}/messages/${channel}`);
     return messages;
   }
 }
